da51039eba28af08296c34457b2f9b6111b83015
Juan Lopez <jlsevasion@MacBook-Pro-de-Juan-3.local>
2023-08-15 17:25:56 +0200

    fist commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const amount = 7
+
+if (amount < 10) {
+    console.log('small number');
+} else {
+    console.log('large number');
+
+}
+
+console.log('hey its my first node app');
+
+console.log(__dirname);
+setInterval(() => {
+    console.log('hello world')
+},1000);
+
